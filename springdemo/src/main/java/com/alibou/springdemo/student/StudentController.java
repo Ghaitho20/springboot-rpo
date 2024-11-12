6dfb6670e994483f8f2b4cb33a627fb4a8fe72a9
Ghaitho20 <151476634+Ghaitho20@users.noreply.github.com>
2024-11-12 23:48:56 +0000

    3rd commit

--- springdemo/src/main/java/com/alibou/springdemo/student/StudentController.java
+++ springdemo/src/main/java/com/alibou/springdemo/student/StudentController.java
@@ -1,19 +1,31 @@
 package com.alibou.springdemo.student;
-
+import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
+
+    private StudentService service ; //= new .... ;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public List<String> findAllStudents() {
-        return List.of(
-                "Alibo",
-                "hello world"
-        );
+
+    public List<student> findaAllStudents() {
+        return service.findaAllStudents() ;
+
+
+
+
     }
 }
